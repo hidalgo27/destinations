e268d33d827b241b2d5405c140062d3e729aebbe
Hidalgo <hidalgochpnce@gmail.com>
2017-08-24 20:12:22 -0500

    Se creo la version responsive table

--- resources/views/layouts/page/default.blade.php
+++ resources/views/layouts/page/default.blade.php
@@ -5,7 +5,7 @@
     <title>Peruvian Destiations | Peru Travel Packages</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
 
     <!-- Loading Bootstrap -->
     <link href="{{mix('css/app.css')}}" rel="stylesheet">
@@ -285,5 +285,15 @@
 
 </script>
 <!-- end olark code -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-105312358-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
